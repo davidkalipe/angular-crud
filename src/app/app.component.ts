b7b5fa2b8de8a86526010cfa711e96deea62ef4e
jackboy14 <kalipedavid@gmail.com>
2024-07-04 12:12:56 +0000

    hi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Book} from "../models/book.model";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'CRUD-ANGULAR';
+  http = inject(HttpClient);
+
+  books$ = this.getAllBooks();
+
+  private getAllBooks(): Observable<Book[]>{
+    return  this.http.get<Book[]>('https://localhost:8080/getAllBooks');
+
+  }
 }
